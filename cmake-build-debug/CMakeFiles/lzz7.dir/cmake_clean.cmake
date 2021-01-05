c0d83ba46a11b06a41a9cef4837e04d0019d693f
Netanel Shimoni <57719538+NetanelShimoni@users.noreply.github.com>
2021-01-05 11:24:19 +0200

    lzz77 - c code

---
+++ cmake-build-debug/CMakeFiles/lzz7.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lzz7.dir/main.c.obj"
+  "lzz7.exe"
+  "lzz7.exe.manifest"
+  "lzz7.lib"
+  "lzz7.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lzz7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
