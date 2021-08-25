9de51556fd4a9842dfab478699983f3ab79958dc
Netanel Shimoni <57719538+NetanelShimoni@users.noreply.github.com>
2021-08-25 18:45:43 +0300

    Add files via upload

---
+++ PPMD/README.md
@@ -0,0 +1,11 @@
+# PPMDJ
+PPMD variation j code from Dmitry Shkarin.
+Small adaptations in the code in order to run on x86-64 linux systems were made.
+
+To compile the code just run
+
+```
+make
+```
+
+
---
+++ PPMD/cmake-build-debug/CMakeFiles/Project_final.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_final.dir/main.cpp.obj"
+  "Project_final.exe"
+  "Project_final.exe.manifest"
+  "Project_final.lib"
+  "Project_final.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_final.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
